c5a43a7cae25d99b03da3ea91f146751a4f244d7
kerbeans <837663641@qq.com>
2023-03-12 08:21:34 +0800

    search panel

--- src/MainPage.js
+++ src/MainPage.js
@@ -1,17 +1,26 @@
 import SearchPanel from "./components/SearchPanel";
 import ControlPanel from "./components/ControlPanel";
 import Testpanel from "./components/Testpanel";
-import { Layout, Space, FloatButton } from "antd";
+import { Layout, Space, FloatButton, Alert } from "antd";
 import { QuestionCircleOutlined, SyncOutlined } from '@ant-design/icons';
 import React from "react";
 
 
 const dummyListData=[
-    {id:1,fileName:'zeghtti.pcd'},
-    {id:2,fileName:'globalMap.pcd'},
-    {id:3,fileName:'mushapotei.pcd'},
-    {id:4,fileName:'npx.lac'}
+    {id:1,fileName:'zeghtti.pcd',filePath:'/abc/zeghtti.pcd'},
+    {id:2,fileName:'globalMap.pcd',filePath:'/ddd/globalMap.pcd'},
+    {id:3,fileName:'mushapotei.pcd',filePath:'mushapotei.pcd'},
+    {id:4,fileName:'npx.lac',filePath:'/bs/npx.lac'}
 ]
+const dummyworkingFiles=[
+    {id:5,fileName:'zeghtti2.pcd',filePath:'/abc/zeghtti.pcd',display:false},
+    {id:6,fileName:'globalMap2.pcd',filePath:'/ddd/globalMap.pcd',display:true},
+    {id:7,fileName:'mushapotei2.pcd',filePath:'mushapotei.pcd',display:false},
+    {id:8,fileName:'npx.lac2',filePath:'/bs/npx.lac',display:false}
+]
+
+
+
 const {Sider, Content } = Layout;
 
 const contentStyle = {
@@ -25,6 +34,7 @@ const siderStyle = {
     textAlign: 'center',
     lineHeight: '120px',
     minHeight: 360,
+    minWidth:360,
     color: '#fff',
     backgroundColor: '#252526',
 };
@@ -32,62 +42,44 @@ const siderStyle = {
 class MainPage extends React.Component{
     constructor(){
         super();
-        this.state={searchList:[],displayedItem:{fileType:'pcd',fileName:''},message:''};
-        this.selectButtonHandler=this.selectButtonHandler.bind(this);
-        this.searchButtonHandler=this.searchButtonHandler.bind(this);
-        this.handleFileChangeLocal=this.handleFileChangeLocal.bind(this);
+        this.state={serverFiles:[],displayedItem:{fileType:'pcd',fileName:''},workingFiles:[],message:''};
+        this.init()
+    }
+    init(){
+   
+        this.loadData=this.loadData.bind(this);
+        this.filterServerFiles=this.filterServerFiles.bind(this);
         this.uploadFile=this.uploadFile.bind(this);
+    }
+    filterServerFiles(searchKey){
+        const original=dummyListData;
+        var filteredList=[];
+        for(var i in original){
+            if(original[i].fileName.search(searchKey)+1)
+                filteredList.push(original[i]);
+        }
+        this.setState({serverFiles:filteredList});
     }
     componentDidMount(){
         this.loadData(dummyListData);
     }
     loadData(listData){
         // load data
-        this.setState({searchList:listData});
+        this.setState({serverFiles:listData});
     }
-    searchButtonHandler(){
-
-    }
-    selectButtonHandler(item){
-        this.setState({displayedItem:item});
-    }
-    uploadFile(){
+    
+    uploadFile(item){
         //  first add to searchList for displaying 
-        if(this.state.displayedItem.fileName===''){
-                ;
-        }
-        else{
-            const id =this.state.searchList.length+1;
-            const item={
-                id:id,
-                fileName:this.state.displayedItem.fileName
+        for(var i in this.state.serverFiles){
+            if(item.id===this.state.serverFiles[i].id){
+                alert('file unsaved');
+                return false;
             }
-            this.setState({
-                searchList:[... this.state.searchList, item]
-            })
         }
+        this.setState({serverFiles:[...this.state.serverFiles,item]});
+        return true;
     }
-    openFromLocal(){
-        const localFile=document.getElementById('read_file');
-        localFile.click();
-    }
-    saveToLocal(){
-
-    }
-    handleFileChangeLocal(event){
-        const filePath = event.target.value; 
-        var fileName=filePath.split('\\');
-        fileName=fileName[fileName.length-1];
-
-        var fileType=fileName.split('.');
-        fileType=fileType[fileType.length-1];
-        this.setState({message:String(fileName)});
-        this.setState({displayedItem:{fileType:fileType,fileName:filePath}});
-    }
-
-
     render(){
-        //console.log('?',this.props.searchList);
         return <div>
                 <Space
                 direction="vertical"
@@ -98,7 +90,6 @@ class MainPage extends React.Component{
             >
                 <Layout>
                     <Sider  width={'10%'} height={'100%'} style={siderStyle}>
-                        <h1>open4vision</h1>
                     </Sider>
                     <Layout height={'100%'}>
                         <Content style={contentStyle}>
@@ -121,7 +112,11 @@ class MainPage extends React.Component{
                 <div style={{"width":"30%","display":'inline',"float":"left","backgroundColor":'red'}} id="test1">
                 {/* write the search list here searchPanel*/}
                     <h1>dummy search list</h1>
-                    < SearchPanel searchList={this.state.searchList} searchButtonHandler={this.searchButtonHandler} selectButtonHandler={this.selectButtonHandler}/>
+                    < SearchPanel serverFiles={this.state.serverFiles} 
+                    workingFiles={this.state.workingFiles} 
+                    filterServerFiles={this.filterServerFiles}
+                    uploadFile={this.uploadFile}
+                    />
                 </div>
                 <div style={{"width":"70%","display":'inline',"float":"left","backgroundColor":"green"}}>
                     {/* write the control panel here controlPanel */}
@@ -132,13 +127,6 @@ class MainPage extends React.Component{
             <div style={{"clear":"both","backgroundColor":"yellow"}}>
                 {/* write the foot print here, and the fromfile, upload, savetolocal button*/}
                 <h1 >dummy footprint</h1>
-                <label >{this.state.message}</label>
-                <br/>
-                <input style={{display: 'none'}} id="read_file" type="file" onChange={this.handleFileChangeLocal}/>
-                <button onClick={this.openFromLocal}>open from local</button>
-                <button onClick={this.uploadFile}>upload to server</button>
-                <input style={{display: 'none'}} id="save_file" type="file" onChange={this.handleFileChangeLocal}/>
-                <button onClick={this.saveToLocal}>download</button>
             </div>
         </div>
     }
--- src/components/SearchPanel.js
+++ src/components/SearchPanel.js
@@ -1,104 +1,195 @@
-import React from "react";
+import {memo, useState, useCallback,useMemo} from 'react'
+import { DndProvider, useDrag, useDrop } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
-class ResultShower extends React.Component{
-    constructor(props){
-        super(props);
+
+
+
+// props.fileToWorkspaceHandler # drag file from server to workspace
+// props.fileUploadHandler # drag file from workspace to server
+// props.closeFileHandler # close the working file
+// props.tickDisplayHandler # set display attribute on wokingFiles
+// props.tickIdx # select which to be editted on
+
+
+
+
+const dummyStyle = {
+    border: "1px dashed gray",
+    padding: "0.5rem",
+    margin: "0.5rem",
+    backgroundColor: "orange"
+  };
+
+
+
+const WorkspaceItem= memo(function WorkspaceItem({info,children}){
+    const [{ isDragging }, drag] = useDrag(
+      () => ({
+        type: 'work',
+        item:{info},
+        collect: (monitor) => ({
+          isDragging: monitor.isDragging()
+        })
+      }),
+      [info]
+    );
+    const style = useMemo(
+      () => ({
+        ...dummyStyle,
+        backgroundColor:'lightyellow',
+        opacity: isDragging ? 0.4 : 1,
+        cursor: "move"
+      }),
+      [isDragging]
+    );
+    return (
+      <div ref={drag} style={style}>
+        <input type="checkbox"/>
+        <small>{info.id} </small>
+        <small>{info.fileName}*</small>
+      </div>
+    );
+});
+
+const SearchItem=memo(function SearchItem({info}){
+    const [{ isDragging }, drag] = useDrag(
+        () => ({
+          type: 'search',
+          item:{info},
+          canDrag: true,
+          collect: (monitor) => ({
+            isDragging: monitor.isDragging()
+          })
+        }),
+        [info]
+      );
+    const style={
+        ...dummyStyle,
+        opacity:isDragging?0.4:1,
+        cursor:"move"
     }
-    render(){
-        var showItem=[]
-        if(this.props.showItem===undefined){
-            ;
-        }
-        else{
-            if(this.props.searchKey===''){
-                showItem=this.props.showItem.map((item,idx)=>
-                <tr key={idx}>
-                    <td key={"id"+String(idx)}>{item.id}</td> 
-                    <td key={"path"+String(idx)}>{item.fileName}</td>
-                </tr>)
-            }
-            else{
-                for(var i in this.props.showItem){
-                    if(this.props.showItem[i].fileName.search(this.props.searchKey)+1){
-                        showItem.push({id:this.props.showItem[i].id,fileName:this.props.showItem[i].fileName});
-                    }
-                }
-                showItem=showItem.map((item,idx)=>
-                <tr key={idx}>
-                    <td key={"id"+String(idx)}>{item.id}</td> 
-                    <td key={"path"+String(idx)}>{item.fileName}</td>
-                </tr>)
-            }
-        }
-        
-        return (
-            <tbody>
-                {showItem}
-            </tbody>
-        );
+    return (
+        <div ref={drag} style={style} role={'SourceBox'}>
+        <small>{info.id} </small>
+        <small>{info.fileName}</small>
+    </div>
+    );
+});
+
+const Container=memo( function Container({onDrop,contents,accept}){
+    const [{isOver},drop]= useDrop(()=>({
+        accept: accept,
+        drop(_item,monitor){
+            onDrop(monitor.getItem());
+            return undefined;
+        },
+        collect:(monitor)=>({
+            isOver:monitor.isOver()
+        })
+    }),[onDrop]);
+
+    let backgroundColor=isOver?'lightblue':'#fff';
+    const style={
+        ...dummyStyle,
+        backgroundColor:backgroundColor
     }
+    return (
+        <div ref={drop}
+            style={style}
+        >
+          {contents}
+
+        </div>
+
+    )
+});
+
+function deleteItem(item,list,key){
+  let newList=[];
+  console.log('init',item,list);
+  for(var i in list){
+    console.log(item[key],list[i][key],'what');
+    if(item[key]!=list[i][key]){
+      newList.push(list[i]);
+    }
+  }
+  console.log(newList,'newlist');
+  return newList;
 }
 
 
 
-class SearchPanel extends React.Component{
-    constructor(props){
-        super(props);
-        this.spSelectHandler=this.spSelectHandler.bind(this);
-        this.state={searchKey:''};
-        this.spSearch=this.spSearch.bind(this);
-    }
-    spSearch(e){
-        e.preventDefault();
-        const form =document.forms.searchPCD;
-        const fileName=form.searchPanel_fileName.value;
 
-        if(fileName==''){
-            this.setState({searchKey:''});
-        }
-        else{
+export default memo(function SearchPanel(props){
 
-            this.setState({searchKey:fileName});
-        }
-    }
-    spSelectHandler(id){
-        for(var i in this.props.searchList){
-            const iter =this.props.searchList[i];
-            if(id===iter.id){
-                this.props.selectButtonHandler(id);
-                break;
-            }
-        }
-    }
-    
-    render(){
-       // console.log(this.state.showItem);
+    const [workingFiles, setWrokingFiles]=useState([]);
 
-        return (<div>
+    const toWorkspace=useCallback((item)=>{
+      const newitem={...item.info,display:false}
+      for(var i in workingFiles){
+        if(newitem.id===workingFiles[i].id)
+          return undefined;
+      }
+      setWrokingFiles([...workingFiles,newitem]);
+    },[workingFiles]);
+
+    const toServer=useCallback((item)=>{
+      const newItem={
+        id:item.info.id,
+        fileName:item.info.fileName,
+        filePath:item.info.filePath
+      };
+      if(props.uploadFile(newItem))
+        setWrokingFiles(deleteItem(newItem,workingFiles,'id'));
+
+    },[workingFiles])
+
+    return(
+        <div className="left">
+        <DndProvider backend={HTML5Backend}>
+            <label>workspace </label>
+
+            <input style={{display: 'none'}} id="read_file" type="file" onChange={(event)=>{
+              const filePath = event.target.value; 
+              var fileName=filePath.split('\\');
+              fileName=fileName[fileName.length-1];        
+              var fileType=fileName.split('.');
+              fileType=fileType[fileType.length-1];
+              const item={
+                id:Math.floor(Math.random()*1000),
+                fileName:fileName,
+                filePath:filePath,
+                display:false
+              }
+              setWrokingFiles([...workingFiles,item]);
+
+            }}/>
+            <button onClick={()=>{
+                  const localFile=document.getElementById('read_file');
+                  localFile.click();
+              }}>open</button>
+            <Container 
+              onDrop={toWorkspace}
+              contents={workingFiles.map((item,idx)=>(<WorkspaceItem info={item} key={idx} />))}
+              accept={'search'}
+             />
             <div>
-                <h3>place for merge</h3>    
-            </div> 
-            <div>
-                {/* search panel */}
-                <form name='searchPCD' onSubmit={this.spSearch}>
-                    <input type="text" name="searchPanel_fileName"></input>
-                    <button>search</button>
+                {/* server list  */}
+                <form name='keywordFilter' onSubmit={(e)=>{
+                    e.preventDefault();
+                    const txt= document.forms.keywordFilter.keywordFilter_text.value;
+                    props.filterServerFiles(txt);
+                }}>
+                    <input type="text" name="keywordFilter_text"></input>
+                    <button>btn</button>
                 </form>
+                <Container 
+                  onDrop={toServer}
+                  contents={props.serverFiles.map((item,idx)=>(<SearchItem info={item} key={idx}></SearchItem>))}
+                  accept={'work'}
+                />
             </div>
-            <div>
-                {/* show result */}
-                <table name='serverFile'>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>FileName</th>
-                        </tr>
-                    </thead>
-                        <ResultShower showItem={this.props.searchList} searchKey={this.state.searchKey} spSelectHandler={this.spSelectHandler}/>
-                </table>
-            </div>
-
-        </div>);
-    }
-}
-export default SearchPanel;
+        </DndProvider>
+      </div>)
+})
